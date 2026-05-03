4faec548d92cca209e970a0b6e668c9d63bf4321
Garcia Luca <Luca.Garcia@alu.frt.utn.edu.ar>
2026-05-02 23:43:54 -0300

    fix: resolucion del problema 2

---
+++ Dsw2026Ej8/Problema2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class Problema2
+    {
+        public string CrearResumenVenta(long productCode,string productDescription,int quantity,decimal unitPrice)
+        {
+            decimal totalCalculado = 0;
+            if(quantity > 0)
+            {
+                totalCalculado = quantity * unitPrice;
+            }
+
+            var miResumen = new
+            {
+                Code = productCode,
+                Description = productDescription,
+                Quantity = quantity,
+                Total = totalCalculado
+            };
+            return $"{miResumen.Code}-{miResumen.Description}-{miResumen.Total}";
+        }
+    }
+}
--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // prueba problema2...
+
+            var ej2 = new Problema2();
+            // Prueba con cantidad positiva
+            Console.WriteLine(ej2.CrearResumenVenta(500, "Monitor", 2, 150));
+            // Resultado esperado: 500-Monitor-300
+
+            // Prueba con cantidad negativa (debe dar total 0)
+            Console.WriteLine(ej2.CrearResumenVenta(501, "Mouse", -5, 20));
+            // Resultado esperado: 501-Mouse-0
         }
     }
 }
